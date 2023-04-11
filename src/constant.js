7f3ef12b3c276064f5d03fa2b11e8022c212827f
hammed noibi <hnobi08@yahoo.com>
2023-04-11 15:39:39 +0100

    modify api base url

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
-export const BaseUrl = { herokulink: 'https://your-diary.herokuapp.com/api/v1/' };
+// export const BaseUrl = { herokulink: 'https://my-diary-ncam.onrender.com/api/v1/' };
+export const BaseUrl = { herokulink:
+ 'http://mydiaryelasticbeanstack-env.eba-vpnaymsi.us-east-1.elasticbeanstalk.com/api/v1/' 
+ };
